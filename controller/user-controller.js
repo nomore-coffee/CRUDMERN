2dbf4ff30f73ae0a7c0fce5af79cfac667a00c71
aman_sharma@archstore.in-at-481412860284 <aman_sharma@archstore.in>
2023-04-24 17:20:11 +0530

    changes

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,120 +1,77 @@
-import express from "express"
-import { response } from "express";
-import { request } from "express";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-import User from "../model/user-schema.js"
+import User from "../model/user-schema.js";
 import signup from "../model/signupinfo-schema.js";
 
+export const getUser = async (request, response) => {
+  // response.status(200).json("HEY CONNECTED")not wworking with this
+  try {
+    let user = await signup.find();
+    response.json({ data: user, statusCode: 200 });
+  } catch (error) {
+    response.json({ message: error.message, statusCode: 403 });
+  }
+};
 
-export const getUser = async(request , response)=>{
-    // response.status(200).json("HEY CONNECTED")not wworking with this
-    try {
-         let user = await User.find();
-         response.json(user)
-     } catch (error) {
-         response.json({message: error.message})
-         
-     }
+export const edituser = async (request, response) => {
+  const user = request.body;
+  const edituser = new User(user);
+
+  try {
+    await User.updateOne({ _id: request.params.id }, edituser);
+    response.json({ message: "User Edited succesfful", statusCode: 200 });
+  } catch (error) {
+    response.json({ message: error.message, statusCode: 403 });
+  }
+};
+
+export const deleteuser = async (request, response) => {
+  try {
+    await User.deleteOne({ _id: request.params.id });
+    response.json({ message: "User deleted succesfful", statusCode: 200 });
+  } catch (error) {
+    response.json({ message: error.message, statusCode: 403 });
+  }
+};
+
+export const signupuser = async (request, response) => {
+var emailRegex = /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+let validCheck = emailRegex.test(request.body.email)
+if(!validCheck){
+    return response.json({ message:"Email Not in proper Format", statusCode: 403 });
 }
+  const user = request.body;
+  console.log("EFSFAS", user);
+  const newUser = new signup(user);
+  console.log("EFSFAS", newUser);
+  try {
+    await newUser.save();
+    return response.json({ status: "ohk", statusC0de: 200 });
+  } catch (error) {
+    // response.json({error:'Dublicate' , status:'error'})
+    return response.json({ message: error.message, statusCode: 403 });
+  }
+};
 
-export const addemp =async(request , response)=>{
-    const user = request.body;
-    const newUser = new User(user);
+export const getlogin = async (request, response) => {
+  try {
+    let user1 = await signup.findOne({
+      username: request.body.username,
+      password: request.body.password,
+    });
 
-    try {
-       await newUser.save();
-        response.json(newUser)
-    } catch (error) {
-        response.json({message: error.message})
-        
+    if (user1) {
+      const token = jwt.sign(
+        {
+          email: user1.username,
+        },
+        "AMANJWT"
+      );
+      return response.json({ status: "ohk", user: token, statusCode: 200 });
+    } else {
+      return response.json({ status: "notohk", user: false, statusCode: 403 });
     }
-}
-
-export  const getuserbyID= async  (request, response)=>{
-    const id = request.params.id; 
-   
-    try {
-        const user = await User.findById(id);
-        response.json(user)
-    } catch (error) {
-        response.json({message:error.message})
-    }
-}
-
-export const edituser = async(request , response)=>{
-    const user=request.body;
-    const edituser= new User(user);
-
-    try {
-        await User.updateOne({_id: request.params.id},edituser)
-        response.json
-    } catch (error) {
-        response.json({message:error.message})
-        
-    }
-}
-
-export const deleteuser = async(request , response)=>{
-
-    try {
-        await User.deleteOne({_id:request.params.id});
-        response.json("User deleted succesfful");
-    } catch (error) {
-        response.json({message:error.message})   
-    }
-}
-
-export const signupuser =async(request , response)=>{
-    const user = request.body;
-    console.log("EFSFAS",user)
-    const newUser = new signup(user);
-    console.log("EFSFAS",newUser)
-
-    // try {
-    // //    await newUser.save();
-    // //     response.json(newUser)
-    // await signup.create({
-    //     name:req.body.name,
-    //     username:req.body.username,
-    //     email:req.body.email,
-    //     password:req.body.password,
-    // })
-    //     res.json({status:'ohk'})
-    // } catch (error) {
-    //     response.json({error:'Dublicate' , status:'error'})
-        
-    // }
-    try {
-        await newUser.save();
-        response.json({status:'ohk'})
-     } catch (error) {
-        // response.json({error:'Dublicate' , status:'error'})
-        response.json({message: error.message})
-         
-     }
-}
-
-export const getlogin = async(request , response)=>{
-
-    try {
-         let user1 = await signup.findOne({
-                username:request.body.username
-                // password:request.body.password
-         })
-
-         if(user1){
-            const token = jwt.sign({
-               email:user1.username
-            },'AMANJWT')
-           return response.json({ status:'ohk',user:token})
-       }  
-       else{
-           return response.json({ status:'notohk',user:false})
-           
-       }
-     } catch (error) {
-         response.json({message: error.message})
-         
-     }
-}
+  } catch (error) {
+    response.json({ message: error.message });
+  }
+};
--- model/signupinfo-schema.js
+++ model/signupinfo-schema.js
@@ -2,8 +2,11 @@ import Mongoose from 'mongoose';
 import autoIncrement from 'mongoose-auto-increment';    
 const signuppinfoSchema  = Mongoose.Schema({
     name:{type:String , required:true},
+    address:{type:String , required:true},
+    mobile_no:{type:Number , required:true},
+    qualification:{type:String , required:true},
     email:{type:String , required: true , unique: true},
-    username:{type:String , required: true },
+    username:{type:String , required: true , unique: true},
     password:{type:String , required:true},
 
 },{collection:'signupinfos'})
